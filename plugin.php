de8cbd62cd6f30837368e3b1e90369e6f5e3c618
Jakob Fix <jakob@jfix.com>
2017-09-06 23:10:41 +0200

    Initialized plugin.php file.

--- plugin.php
+++ plugin.php
@@ -1,25 +1,14 @@
 <?php
 /*
-Plugin Name: Your Plugin Name
-Plugin URI: http://github.com/your_name/your_plugin
-Description: One line description of your plugin
+Plugin Name: Strict Keyword Checker
+Plugin URI: https://github.com/jfix/yourls-strict-keyword-checker
+Description: Make sure a custom keyword doesn't contain a reserved word
 Version: 1.0
-Author: Your Name
-Author URI: http://your-site-if-any/
+Author: Jakob Fix
+Author URI: https://github.com/jfix
 */
 
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-/*
-
- Your code goes here.
- 
- Suggested read:
- https://github.com/YOURLS/YOURLS/wiki/Coding-Standards
- https://github.com/YOURLS/YOURLS/wiki#for-developpers
- https://github.com/YOURLS/YOURLS/wiki/Plugin-List#get-your-plugin-listed-here
- 
- Have fun!
- 
-*/
+// now I just have to write the code ....
