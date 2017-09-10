61ffead6dc0d704e685aebffeb42d70708b8b955
Jakob Fix <jakob@jfix.com>
2017-09-10 15:57:12 +0200

    :sparkles: Initial version of plugin ready.

--- plugin.php
+++ plugin.php
@@ -11,9 +11,23 @@ Author URI: https://github.com/jfix
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
+// Associate filter function with hook 'shunt_add_new_link'
 yourls_add_filter( 'shunt_add_new_link', 'strict_check_keyword' );
 
-function strict_check_keyword( $original_keyword ) {
-  // now I just have to write the code ....
+// $pre = yourls_apply_filter( 'shunt_add_new_link', false, $url, $keyword, $title );
+// The filter function has access to all parameters listed above
 
+function strict_check_keyword( $return, $url, $original_keyword, $title ) {
+  global $yourls_reserved_URL;
+
+  foreach ($yourls_reserved_URL as $keyword) {
+    if (preg_match("/$keyword/i", $original_keyword)) {
+      $return['status'] = 'fail';
+      $return['code'] = 'error:keyword';
+      $return['message'] = yourls_s( 'This keyword may not be used.' );
+      $return['errorCode'] = '400';
+      return $return;
+    }
+  }
+  return false;
 }
